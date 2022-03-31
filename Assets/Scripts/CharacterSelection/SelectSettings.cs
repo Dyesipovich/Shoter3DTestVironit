fa82a43f2c5999742db01fa14a1c6888fdc0a017
Dyesipovich <99168622+Dyesipovich@users.noreply.github.com>
2022-03-31 15:23:43 +0300

    menu

--- Assets/Scripts/CharacterSelection/SelectSettings.cs
+++ Assets/Scripts/CharacterSelection/SelectSettings.cs
@@ -2,18 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SelectSettings : MonoBehaviour
+
+public class SelectSettings : MonoBehaviour, IDragHandler
 {
-    [SerializeField] private Toggle[] _selectTeam;
+    [SerializeField] private Toggle[] _selectTeamToggle;
+    [SerializeField] private GameObject[] _selectPerson;
+    private bool _isFirstTeam;
 
     private void Update()
     {
-        _selectTeam[0].isOn = true;
+        SelectTeam();
     }
 
     private void SelectTeam()
     {
-
+        if (_selectTeamToggle[0].isOn)
+        {
+            _selectPerson[0].SetActive(true);
+            _selectPerson[1].SetActive(false);
+            _isFirstTeam = true;
+        }
+        else
+        {
+            _selectPerson[0].SetActive(false);
+            _selectPerson[1].SetActive(true);
+            _isFirstTeam = false;
+        }
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (_isFirstTeam)
+        {
+            _selectPerson[0].transform.Rotate(0f, -eventData.delta.x, 0f);
+        }
+        else
+        {
+            _selectPerson[1].transform.Rotate(0f, -eventData.delta.x, 0f);
+        }
+    }
+
 }
